9dd9e1d3a8d42984a1ce7f799999e444c2d7fec8
nablus1 <nablusmirera1@gmail.com>
2023-05-03 23:04:51 +0300

    first commit

---
+++ Script.py
@@ -0,0 +1,7 @@
+character_name ="Nablus mirera"
+character_age ="19"
+print("Hello\nMy name is "+character_name+"" )
+print("I am " +character_age+"years old")
+character_name ="Jomokenyata University of Agriculture and Technology"
+print("Am currently studying at " + character_name + "")
+print(f'Hello World')
---
+++ grading.py
@@ -0,0 +1,25 @@
+#script to print student name and each subject score
+def student_score():
+ name = (input('Enter your name :\n')) 
+ marks = int(input('Enter your score 1 : '))
+ biology_grade = 68 
+ grade = ""
+ if marks > 75 :
+  grade = 'A'
+ elif marks >= 70 and marks < 75:
+  grade = 'A-'
+ elif marks >= 69 and marks < 70:
+  grade = 'B+'
+ elif marks >= 62 and marks < 69:
+  grade = 'B'
+ elif marks >= 55 and marks < 62:
+  grade = 'B-'
+ elif marks >= 46 and marks < 55:
+  grade = 'C+'
+
+
+ print('Student name:' + name )
+ print('Subjects:' + 'Biology:' , biology_grade)
+ print('Subject:' +  grade )
+
+student_score()
